d350f586cfbd6d128b823024eafc1cd71d1feebf
srishtiv03 <srishtiv03@gmail.com>
2025-06-20 16:54:49 +0530

    yo

--- app/layout.js
+++ app/layout.js
@@ -1,6 +1,8 @@
 import { Inter } from "next/font/google";
 import "./globals.css";
 import { headers } from "next/headers";
+import Header from "@/components/header";
+import { ClerkProvider } from "@clerk/nextjs/dist/types/components.server";
 
 const inter = Inter({subsets: ["latin"]});
 
@@ -11,17 +13,22 @@ export const metadata = {
 
 export default function RootLayout({ children }) {
   return (
-    <html lang="en">
-      <body className={`${inter.className}`}>
-        {/* header */}
-        <main className="min-h-screen">{children}</main>
-        {/* footer */}
-        <footer className="bg-blue-50 py-12">
-          <div className="container mx-auto px-4 text-center text-gray-600">
-            <p>Crafted with care by Srishti Verma</p>
-          </div>
-        </footer>
-        </body>
-    </html>
+    <ClerkProvider>
+
+    
+      <html lang="en">
+        <body className={`${inter.className}`}>
+          {/* header */}
+          <Header></Header>
+          <main className="min-h-screen">{children}</main>
+          {/* footer */}
+          <footer className="bg-blue-50 py-12">
+            <div className="container mx-auto px-4 text-center text-gray-600">
+              <p>Crafted with care by Srishti Verma</p>
+            </div>
+          </footer>
+          </body>
+      </html>
+    </ClerkProvider>
   );
 }
