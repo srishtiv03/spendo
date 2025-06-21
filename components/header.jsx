1fe6e32c13902ae0e2de78b065da114addb3e3f2
srishtiv03 <srishtiv03@gmail.com>
2025-06-21 14:06:58 +0530

    cdcjdc

--- components/header.jsx
+++ components/header.jsx
@@ -1,14 +1,14 @@
 import { SignedIn, SignedOut, SignInButton, SignUpButton, UserButton } from "@clerk/nextjs"
-import { Link } from "next/link";
+import Link from "next/link";
 import Image from "next/image";
 
 const Header = () => {
   return (
     <div className="fixed top-0 w-full bg-white/80 backdrop-blur-md z-50 border-b">
       <nav className="container mx-auto px-4 py-4 flex items-center justify-between">
-        <<Link href="/">
+        <Link href="/">
         <Image src={"/logo.png"} alt="spendo logo" width={200} height={60} className="h-12 w-auto object-contain" />
-        </Link>>
+        </Link>
       
         <SignedOut>
             <SignInButton />
